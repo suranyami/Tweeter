4da51ad8a741cd55e5751805d9bfd93153f48794
David Parry <david.parry@suranyami.com>
2012-03-11 14:18:18 +1100

    Initial Commit

---
+++ PRPFirstProjectTweeter/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  PRPFirstProjectTweeter
+//
+//  Created by David Parry on 11/03/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
